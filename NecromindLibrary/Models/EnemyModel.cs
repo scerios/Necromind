9c75fc952d5e8d5070986032447ef861e5dcc06d
Takacs Robert <89.t.robert@gmail.com>
2021-03-23 17:18:39 +0100

    feat(EnemyModel): implemented IAdminSetter

--- NecromindLibrary/Models/EnemyModel.cs
+++ NecromindLibrary/Models/EnemyModel.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace NecromindLibrary.Models
 {
-    public class EnemyModel : BaseModel, IFighter
+    public class EnemyModel : BaseModel, IFighter, IAdminSetter
     {
         public int DmgMin { get; private set; }
         public int DmgMax { get; private set; }
@@ -48,11 +48,7 @@ namespace NecromindLibrary.Models
 
         public void TakeDmg(IFighter enemy)
         {
-            //Health -= enemy.Dmg - Def;
-            //if (Health < 1)
-            //{
-            //    Die(enemy);
-            //}
+            // TODO - Implement take dmg logic.
         }
 
         public void Die(IFighter enemy)
@@ -62,6 +58,42 @@ namespace NecromindLibrary.Models
 
             var traderPlayer = enemy as ITrader;
             traderPlayer.RecieveGold(Gold);
+        }
+
+        public void AdminSetName(string name)
+        {
+            Name = name;
+        }
+
+        public void AdminSetGold(int gold)
+        {
+            Gold = gold;
+        }
+
+        public void AdminSetLvl(int lvl)
+        {
+            Lvl = lvl;
+        }
+
+        public void AdminSetDmgMin(int dmg)
+        {
+            DmgMin = dmg;
+        }
+
+        public void AdminSetDmgMax(int dmg)
+        {
+            DmgMax = dmg;
+        }
+
+        public void AdminSetDef(int def)
+        {
+            Def = def;
+        }
+
+        public void AdminSetHealth(int health)
+        {
+            HealthMax = health;
+            Health = health;
         }
     }
 }
