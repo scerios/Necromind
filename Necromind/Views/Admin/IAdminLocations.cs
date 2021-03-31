3a03d7471a0b65e80d15cb028af8f770ff061198
Takacs Robert <89.t.robert@gmail.com>
2021-03-31 14:38:27 +0200

    feat(IAdminLocations): added view

---
+++ Necromind/Views/Admin/IAdminLocations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NecromindUI.Views.Admin
+{
+    public interface IAdminLocations
+    {
+        string CreateName { get; set; }
+        string EditName { get; set; }
+        string CreateDescription { get; set; }
+        string EditDescription { get; set; }
+        bool CreateIsHostile { get; set; }
+        bool EditIsHostile { get; set; }
+        ListBox Locations { get; }
+        ListBox CreateEnemies { get; }
+        ListBox CreateAddedEnemies { get; }
+        ListBox EditEnemies { get; }
+        ListBox EditAddedEnemies { get; }
+        Timer TimHideAdd { get; }
+        Timer TimHideEdit { get; }
+        Label LabEnemyAdd { get; }
+        Label LabEnemyEdit { get; }
+    }
+}
