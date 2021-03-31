8bba5586e551341c0e8bae1106483439d0872b9c
Takacs Robert <89.t.robert@gmail.com>
2021-03-31 21:55:47 +0200

    feat(DBConfig): added static DB related config

---
+++ Necromind/Config/DBConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace NecromindUI.Config
+{
+    public static class DBConfig
+    {
+        public static readonly string HeroesCollection = ConfigurationManager.AppSettings.Get("heroesCollection");
+        public static readonly string VendorsCollection = ConfigurationManager.AppSettings.Get("vendorsCollection");
+        public static readonly string EnemiesCollection = ConfigurationManager.AppSettings.Get("enemiesCollection");
+        public static readonly string LocationsCollection = ConfigurationManager.AppSettings.Get("locationsCollection");
+        public static readonly string MapsCollection = ConfigurationManager.AppSettings.Get("mapsCollection");
+    }
+}
