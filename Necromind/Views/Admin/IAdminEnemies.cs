773d75b17b8332263a9d71305bfb994b9f2dc817
Takacs Robert <89.t.robert@gmail.com>
2021-04-01 19:59:50 +0200

    refactor(IAdminEnemies): changed Add to Create for consistency

--- Necromind/Views/Admin/IAdminEnemies.cs
+++ Necromind/Views/Admin/IAdminEnemies.cs
@@ -5,24 +5,24 @@ namespace NecromindUI.Views.Admin
 {
     public interface IAdminEnemies
     {
-        string AddName { get; set; }
+        string CreateName { get; set; }
         string EditName { get; set; }
-        string AddLvl { get; set; }
+        string CreateLvl { get; set; }
         string EditLvl { get; set; }
-        string AddGold { get; set; }
+        string CreateGold { get; set; }
         string EditGold { get; set; }
-        string AddDmgMin { get; set; }
+        string CreateDmgMin { get; set; }
         string EditDmgMin { get; set; }
-        string AddDmgMax { get; set; }
+        string CreateDmgMax { get; set; }
         string EditDmgMax { get; set; }
-        string AddDef { get; set; }
+        string CreateDef { get; set; }
         string EditDef { get; set; }
-        string AddHealth { get; set; }
+        string CreateHealth { get; set; }
         string EditHealth { get; set; }
         ListBox Enemies { get; }
-        Timer TimHideAdd { get; }
+        Timer TimHideCreate { get; }
         Timer TimHideEdit { get; }
-        Label LabEnemyAdd { get; }
+        Label LabEnemyCreate { get; }
         Label LabEnemyEdit { get; }
     }
 }
