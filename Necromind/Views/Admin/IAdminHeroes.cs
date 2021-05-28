f3ef23d5898eeba522bfbafab1d114c508d0e43d
Takacs Robert <89.t.robert@gmail.com>
2021-05-28 11:25:30 +0200

    feat(IAdminHeroes): added bool getters/setters to the IView

--- Necromind/Views/Admin/IAdminHeroes.cs
+++ Necromind/Views/Admin/IAdminHeroes.cs
@@ -11,6 +11,11 @@ namespace NecromindUI.Views.Admin
         string TbDmgMax { get; set; }
         string TbDef { get; set; }
         string TbHealth { get; set; }
+        bool LabLvlError { get; set; }
+        bool LabGoldError { get; set; }
+        bool LabDmgError { get; set; }
+        bool LabDefError { get; set; }
+        bool LabHealthError { get; set; }
         ListBox LbHeroes { get; }
         Timer TimHide { get; }
         Label LabHeroEdit { get; }
