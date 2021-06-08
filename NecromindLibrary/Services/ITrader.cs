a67757bb874aba77eff3ff51c85c1218f2f9e954
Takacs Robert <89.t.robert@gmail.com>
2021-06-08 10:17:33 +0200

    refactor(): cleaned up code

--- NecromindLibrary/Services/ITrader.cs
+++ NecromindLibrary/Services/ITrader.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NecromindLibrary.Services
 {
     public interface ITrader
     {
         void GiveGold(ITrader trader, int amount);
+
         void RecieveGold(int amount);
     }
 }
