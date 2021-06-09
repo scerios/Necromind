1ba066cd9745f628767d438bb06c3de88c59ad94
Takacs Robert <89.t.robert@gmail.com>
2021-06-09 14:49:05 +0200

    feat(AdminEnemiesPresenter): added input validation logic

--- Necromind/Presenters/Admin/AdminEnemiesPresenter.cs
+++ Necromind/Presenters/Admin/AdminEnemiesPresenter.cs
@@ -1,6 +1,7 @@
 ﻿using NecromindLibrary.Config;
 using NecromindLibrary.Models;
 using NecromindLibrary.Repository;
+using NecromindLibrary.Services;
 using NecromindUI.Config;
 using NecromindUI.Views.Admin;
 using System;
@@ -96,6 +97,174 @@ namespace NecromindUI.Presenters.Admin
             }
         }
 
+        public bool AreCInputsValid()
+        {
+            int errorCount = 0;
+
+            if (IsNameCInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsLvlCInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsGoldCInvalid())
+            {
+                errorCount++;
+            }
+
+            if (AreDmgCInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsDefCInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsHealthCInvalid())
+            {
+                errorCount++;
+            }
+
+            return errorCount == 0;
+        }
+
+        public bool AreMInputsValid()
+        {
+            int errorCount = 0;
+
+            if (IsNameMInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsLvlMInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsGoldMInvalid())
+            {
+                errorCount++;
+            }
+
+            if (AreDmgMInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsDefMInvalid())
+            {
+                errorCount++;
+            }
+
+            if (IsHealthMInvalid())
+            {
+                errorCount++;
+            }
+
+            return errorCount == 0;
+        }
+
+        #region Create errors
+
+        private bool IsNameCInvalid()
+        {
+            _adminEnemies.LabNameCError = !ValidationService.IsValidHeroName(_adminEnemies.TbCreateName);
+
+            return _adminEnemies.LabNameCError;
+        }
+
+        private bool IsLvlCInvalid()
+        {
+            _adminEnemies.LabLvlCError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbCreateLvl);
+
+            return _adminEnemies.LabLvlCError;
+        }
+
+        private bool IsGoldCInvalid()
+        {
+            _adminEnemies.LabGoldCError = !ValidationService.IsGreaterThanOrEqualToZero(_adminEnemies.TbCreateGold);
+
+            return _adminEnemies.LabGoldCError;
+        }
+
+        private bool AreDmgCInvalid()
+        {
+            _adminEnemies.LabDmgCError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbCreateDmgMin) &&
+                ValidationService.IsFirstIsGreaterThanSecond(_adminEnemies.TbCreateDmgMax, _adminEnemies.TbCreateDmgMin);
+
+            return _adminEnemies.LabDmgCError;
+        }
+
+        private bool IsDefCInvalid()
+        {
+            _adminEnemies.LabDefCError = !ValidationService.IsGreaterThanOrEqualToZero(_adminEnemies.TbCreateDef);
+
+            return _adminEnemies.LabDefCError;
+        }
+
+        private bool IsHealthCInvalid()
+        {
+            _adminEnemies.LabHealthCError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbCreateHealth);
+
+            return _adminEnemies.LabHealthCError;
+        }
+
+        #endregion Create errors
+
+        #region Modify errors
+
+        private bool IsNameMInvalid()
+        {
+            _adminEnemies.LabNameMError = !ValidationService.IsValidHeroName(_adminEnemies.TbEditName);
+
+            return _adminEnemies.LabNameMError;
+        }
+
+        private bool IsLvlMInvalid()
+        {
+            _adminEnemies.LabLvlMError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbEditLvl);
+
+            return _adminEnemies.LabLvlMError;
+        }
+
+        private bool IsGoldMInvalid()
+        {
+            _adminEnemies.LabGoldMError = !ValidationService.IsGreaterThanOrEqualToZero(_adminEnemies.TbEditGold);
+
+            return _adminEnemies.LabGoldMError;
+        }
+
+        private bool AreDmgMInvalid()
+        {
+            _adminEnemies.LabDmgMError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbEditDmgMin) &&
+                ValidationService.IsFirstIsGreaterThanSecond(_adminEnemies.TbEditDmgMax, _adminEnemies.TbEditDmgMin);
+
+            return _adminEnemies.LabDmgMError;
+        }
+
+        private bool IsDefMInvalid()
+        {
+            _adminEnemies.LabDefMError = !ValidationService.IsGreaterThanOrEqualToZero(_adminEnemies.TbEditDef);
+
+            return _adminEnemies.LabDefMError;
+        }
+
+        private bool IsHealthMInvalid()
+        {
+            _adminEnemies.LabHealthMError = !ValidationService.IsGreaterThanZero(_adminEnemies.TbEditHealth);
+
+            return _adminEnemies.LabHealthMError;
+        }
+
+        #endregion Modify errors
+
         private void LoadAllEnemies()
         {
             _enemies = _mongoConnector.GetAllRecords<EnemyModel>(DBConfig.EnemiesCollection);
@@ -108,30 +277,7 @@ namespace NecromindUI.Presenters.Admin
             _adminEnemies.LbEnemies.DisplayMember = _combinedName;
         }
 
-        private void SetCreateEnemyProperties()
-        {
-            _enemy = new EnemyModel();
-            _enemy.AdminSetName(_adminEnemies.TbCreateName);
-            _enemy.AdminSetLvl(_adminEnemies.TbCreateLvl);
-            _enemy.AdminSetGold(_adminEnemies.TbCreateGold);
-            _enemy.AdminSetDmgMin(_adminEnemies.TbCreateDmgMin);
-            _enemy.AdminSetDmgMax(_adminEnemies.TbCreateDmgMax);
-            _enemy.AdminSetDef(_adminEnemies.TbCreateDef);
-            _enemy.AdminSetHealth(_adminEnemies.TbCreateHealth);
-            _enemy.AdminSetCombinedName();
-        }
-
-        private void SetEditEnemyProperties()
-        {
-            _enemy.AdminSetName(_adminEnemies.TbEditName);
-            _enemy.AdminSetLvl(_adminEnemies.TbEditLvl);
-            _enemy.AdminSetGold(_adminEnemies.TbEditGold);
-            _enemy.AdminSetDmgMin(_adminEnemies.TbEditDmgMin);
-            _enemy.AdminSetDmgMax(_adminEnemies.TbEditDmgMax);
-            _enemy.AdminSetDef(_adminEnemies.TbEditDef);
-            _enemy.AdminSetHealth(_adminEnemies.TbEditHealth);
-            _enemy.AdminSetCombinedName();
-        }
+        #region Alerts
 
         private void AlertCreateSuccess(string msg)
         {
@@ -169,6 +315,35 @@ namespace NecromindUI.Presenters.Admin
         {
             label.Visible = true;
             timer.Start();
+        }
+
+        #endregion Alerts
+
+        #region UI modifications
+
+        private void SetCreateEnemyProperties()
+        {
+            _enemy = new EnemyModel();
+            _enemy.AdminSetName(_adminEnemies.TbCreateName);
+            _enemy.AdminSetLvl(_adminEnemies.TbCreateLvl);
+            _enemy.AdminSetGold(_adminEnemies.TbCreateGold);
+            _enemy.AdminSetDmgMin(_adminEnemies.TbCreateDmgMin);
+            _enemy.AdminSetDmgMax(_adminEnemies.TbCreateDmgMax);
+            _enemy.AdminSetDef(_adminEnemies.TbCreateDef);
+            _enemy.AdminSetHealth(_adminEnemies.TbCreateHealth);
+            _enemy.AdminSetCombinedName();
+        }
+
+        private void SetEditEnemyProperties()
+        {
+            _enemy.AdminSetName(_adminEnemies.TbEditName);
+            _enemy.AdminSetLvl(_adminEnemies.TbEditLvl);
+            _enemy.AdminSetGold(_adminEnemies.TbEditGold);
+            _enemy.AdminSetDmgMin(_adminEnemies.TbEditDmgMin);
+            _enemy.AdminSetDmgMax(_adminEnemies.TbEditDmgMax);
+            _enemy.AdminSetDef(_adminEnemies.TbEditDef);
+            _enemy.AdminSetHealth(_adminEnemies.TbEditHealth);
+            _enemy.AdminSetCombinedName();
         }
 
         private void ClearAddFields()
@@ -225,5 +400,7 @@ namespace NecromindUI.Presenters.Admin
             _adminEnemies.BtnEditIsEnabled = false;
             _adminEnemies.BtnDelIsEnabled = false;
         }
+
+        #endregion UI modifications
     }
 }
