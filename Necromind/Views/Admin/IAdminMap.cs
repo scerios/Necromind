bed05c7d29bf90b1a892a6dcb08a63c333d8bc0a
Takacs Robert <89.t.robert@gmail.com>
2021-06-14 15:34:46 +0200

    feat(IAdminMap): added interface view to handle map's UI

---
+++ Necromind/Views/Admin/IAdminMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NecromindUI.Views.Admin
+{
+    public interface IAdminMap
+    {
+        string LabX { get; set; }
+        string LabY { get; set; }
+        string LabLocName { get; set; }
+        string LabLocDescription { get; set; }
+        string TbJumpX { get; set; }
+        string TbJumpY { get; set; }
+    }
+}
