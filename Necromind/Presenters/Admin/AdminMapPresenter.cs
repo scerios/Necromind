2a0c6b0c91b729ac4f281c387720594881681e3e
Takacs Robert <89.t.robert@gmail.com>
2021-06-14 15:44:22 +0200

    feat(AdminMapPresenter): added presenter with basic functionality

---
+++ Necromind/Presenters/Admin/AdminMapPresenter.cs
@@ -0,0 +1,18 @@
+﻿using NecromindLibrary.Repository;
+using NecromindUI.Views.Admin;
+using System;
+
+namespace NecromindUI.Presenters.Admin
+{
+    public class AdminMapPresenter
+    {
+        private readonly MongoConnector _mongoConnector;
+        private readonly IAdminMap _adminMap;
+
+        public AdminMapPresenter(IAdminMap adminMap)
+        {
+            _adminMap = adminMap;
+            _mongoConnector = MongoConnector.GetInstance();
+        }
+    }
+}
