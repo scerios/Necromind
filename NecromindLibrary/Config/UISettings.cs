36912e93925871ef89a2194a70b31d5043f8eac1
Takacs Robert <89.t.robert@gmail.com>
2021-06-14 10:29:43 +0200

    feat(UISettings): added new variables for buttons

--- NecromindLibrary/Config/UISettings.cs
+++ NecromindLibrary/Config/UISettings.cs
@@ -11,5 +11,11 @@ namespace NecromindLibrary.Config
         public readonly static Color GreenColor = Color.FromArgb(106, 176, 76);
         public readonly static Color WhiteColor = Color.FromArgb(229, 232, 232);
         public readonly static string FontStyle = "Courier New";
+
+        public readonly static int BtnSizeBigX = 100;
+        public readonly static int BtnSizeSmallX = 25;
+        public readonly static int BtnSizeY = 25;
+        public readonly static int BtnFontSizeBig = 12;
+        public readonly static int BtnFontSizeSmall = 10;
     }
 }
