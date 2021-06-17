fa8b29c156ffd6b0391689c83b5eec4fefbdad29
Takacs Robert <89.t.robert@gmail.com>
2021-06-17 14:19:55 +0200

    feat(MapTileModel): added model to handle mapTiles

---
+++ NecromindLibrary/Models/MapTileModel.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace NecromindLibrary.Models
+{
+    public class MapTileModel
+    {
+        /// <summary>
+        /// ID of the tile.
+        /// </summary>
+        [BsonId]
+        public Guid Id { get; private set; }
+
+        /// <summary>
+        /// Position on the X axis.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Position on the Y axis.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// ID of the held location.
+        /// </summary>
+        public Guid LocationId { get; set; }
+    }
+}
