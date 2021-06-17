e7367faff1eb31370b4ee291d79298ebd9e30cca
Takacs Robert <89.t.robert@gmail.com>
2021-06-17 10:44:05 +0200

    feat(UISettings): added color for labels + renamed properties

--- NecromindLibrary/Config/UISettings.cs
+++ NecromindLibrary/Config/UISettings.cs
@@ -6,10 +6,11 @@ namespace NecromindLibrary.Config
     public static class UISettings
     {
         public readonly static Color BackgroundColor = Color.FromArgb(23, 32, 42);
-        public readonly static Color OrangeColor = Color.FromArgb(211, 84, 0);
-        public readonly static Color RedColor = Color.FromArgb(214, 48, 49);
-        public readonly static Color GreenColor = Color.FromArgb(106, 176, 76);
-        public readonly static Color WhiteColor = Color.FromArgb(229, 232, 232);
+        public readonly static Color LabelColor = Color.FromArgb(191, 64, 50);
+        public readonly static Color ValueColor = Color.FromArgb(211, 84, 0);
+        public readonly static Color ErrorColor = Color.FromArgb(214, 48, 49);
+        public readonly static Color SuccessColor = Color.FromArgb(106, 176, 76);
+        public readonly static Color TextColor = Color.FromArgb(229, 232, 232);
         public readonly static string FontStyle = "Courier New";
 
         public readonly static int BtnSizeBigX = 100;
