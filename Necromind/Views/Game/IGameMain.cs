5f15cdac0eb1b2fba133e403169b993ea0abb222
Takacs Robert <89.t.robert@gmail.com>
2021-06-24 12:01:28 +0200

    feat(IGameMain): added bools to handle movement buttons availabilities

--- Necromind/Views/Game/IGameMain.cs
+++ Necromind/Views/Game/IGameMain.cs
@@ -5,9 +5,14 @@ namespace NecromindUI.Views.Game
     public interface IGameMain
     {
         bool IsPanExitVisible { get; set; }
-        string CurrentLocation { get; set; }
+        bool BtnIsNorthEnabled { get; set; }
+        bool BtnIsSouthEnabled { get; set; }
+        bool BtnIsWestEnabled { get; set; }
+        bool BtnIsEastEnabled { get; set; }
         string HeroName { get; set; }
         RichTextBox EventLog { get; }
+        Label LabLocationName { get; }
+
         Label LabHealthMax { get; }
         Label LabHealth { get; }
         Label LabDmgMin { get; }
