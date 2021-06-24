ddd4838a8e018e18e04080807851248926e02b1e
Takacs Robert <89.t.robert@gmail.com>
2021-06-24 14:45:59 +0200

    feat(UISettings): added TextColorInfo

--- NecromindLibrary/Config/UISettings.cs
+++ NecromindLibrary/Config/UISettings.cs
@@ -10,7 +10,8 @@ namespace NecromindLibrary.Config
         public readonly static Color ValueColor = Color.FromArgb(211, 84, 0);
         public readonly static Color ErrorColor = Color.FromArgb(214, 48, 49);
         public readonly static Color SuccessColor = Color.FromArgb(106, 176, 76);
-        public readonly static Color TextColor = Color.FromArgb(229, 232, 232);
+        public readonly static Color TextColorDefault = Color.FromArgb(229, 232, 232);
+        public readonly static Color TextColorInfo = Color.FromArgb(127, 140, 141);
         public readonly static string FontStyle = "Courier New";
 
         public readonly static int BtnSizeBigX = 100;
