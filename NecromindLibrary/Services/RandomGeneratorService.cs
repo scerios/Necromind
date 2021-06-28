9b08cb31516c90fa3d5e3171325f7f460d3467fe
Takacs Robert <89.t.robert@gmail.com>
2021-06-28 16:44:23 +0200

    feat(RandomGeneratorService): added new service to generate random numbers and to effect game

---
+++ NecromindLibrary/Services/RandomGeneratorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NecromindLibrary.Services
+{
+    public static class RandomGeneratorService
+    {
+        private static readonly Random _rng = new Random();
+        private const int SPAWN_CHANCE_PERCENT = 20;
+
+        public static bool IsEnemySpawned()
+        {
+            return _rng.Next(1, 100 / SPAWN_CHANCE_PERCENT) == 1;
+        }
+
+        public static Guid GetRandomEnemyId(List<Guid> enemies)
+        {
+            return enemies[_rng.Next(1, enemies.Count)];
+        }
+    }
+}
