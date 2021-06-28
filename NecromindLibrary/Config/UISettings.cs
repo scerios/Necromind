7b3fdc22d55759f47b7bd6de66288f4f9cf82e1b
Takacs Robert <89.t.robert@gmail.com>
2021-06-28 10:21:48 +0200

    feat(UISettings): added new colors and MapTileSize

--- NecromindLibrary/Config/UISettings.cs
+++ NecromindLibrary/Config/UISettings.cs
@@ -12,6 +12,8 @@ namespace NecromindLibrary.Config
         public readonly static Color SuccessColor = Color.FromArgb(106, 176, 76);
         public readonly static Color TextColorDefault = Color.FromArgb(229, 232, 232);
         public readonly static Color TextColorInfo = Color.FromArgb(127, 140, 141);
+        public readonly static Color EmptyTileColor = Color.FromArgb(210, 218, 226);
+        public readonly static Color UnaccessibleTileColor = Color.FromArgb(30, 39, 46);
         public readonly static string FontStyle = "Courier New";
 
         public readonly static int BtnSizeBigX = 100;
@@ -19,5 +21,6 @@ namespace NecromindLibrary.Config
         public readonly static int BtnSizeY = 25;
         public readonly static int BtnFontSizeBig = 12;
         public readonly static int BtnFontSizeSmall = 10;
+        public readonly static int MapTileSize = 10;
     }
 }
