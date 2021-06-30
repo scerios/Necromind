3687064158172e099b1c876f53d9a9ca2e1f90e0
Takacs Robert <89.t.robert@gmail.com>
2021-06-30 15:24:54 +0200

    feat(EnemyEventArgs): added new Args to connect enemy with GameMain

---
+++ NecromindLibrary/EventArgs/EnemyEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using NecromindLibrary.Models;
+using System;
+
+namespace NecromindLibrary.EventArgs
+{
+    public class EnemyEventArgs : System.EventArgs
+    {
+        public EnemyModel Enemy { get; private set; }
+
+        public EnemyEventArgs(EnemyModel enemy)
+        {
+            Enemy = enemy;
+        }
+    }
+}
