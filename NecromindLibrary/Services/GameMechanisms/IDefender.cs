677dd7037876d41396fd0270a230591a3db2451b
Takacs Robert <89.t.robert@gmail.com>
2021-07-01 15:32:41 +0200

    feat(IDefender): added OnRested event

--- NecromindLibrary/Services/GameMechanisms/IDefender.cs
+++ NecromindLibrary/Services/GameMechanisms/IDefender.cs
@@ -4,6 +4,8 @@ namespace NecromindLibrary.Services.GameMechanisms
 {
     public interface IDefender
     {
+        event EventHandler OnRested;
+
         /// <summary>
         /// Setup a defensive stance and by raising awareness it gives extra Def.
         /// </summary>
@@ -13,5 +15,10 @@ namespace NecromindLibrary.Services.GameMechanisms
         /// Ends the defensive stance and restores original Def.
         /// </summary>
         void EndFortification();
+
+        /// <summary>
+        /// By becoming rested, hero can gain the defensive stance once again.
+        /// </summary>
+        void BecomeRested();
     }
 }
